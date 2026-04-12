c393746967a380d89e8c4e2ac6f1f4f8f0f8e010
Edinardo Mateus <edinardo.mateus@gmail.com>
2026-04-12 22:51:28 +0000

    Correção readme

--- megastore_busca/target/debug/deps/megastore-d66525ef07236c54.d
+++ megastore_busca/target/debug/deps/megastore-d66525ef07236c54.d
@@ -1,8 +1,8 @@
-/workspaces/MegaStore/megastore-busca/target/debug/deps/megastore-d66525ef07236c54.d: src/lib.rs src/product.rs src/index.rs src/graph.rs src/search.rs
+/workspaces/MegaStore/megastore_busca/target/debug/deps/megastore-d66525ef07236c54.d: src/lib.rs src/product.rs src/index.rs src/graph.rs src/search.rs
 
-/workspaces/MegaStore/megastore-busca/target/debug/deps/libmegastore-d66525ef07236c54.rlib: src/lib.rs src/product.rs src/index.rs src/graph.rs src/search.rs
+/workspaces/MegaStore/megastore_busca/target/debug/deps/libmegastore-d66525ef07236c54.rlib: src/lib.rs src/product.rs src/index.rs src/graph.rs src/search.rs
 
-/workspaces/MegaStore/megastore-busca/target/debug/deps/libmegastore-d66525ef07236c54.rmeta: src/lib.rs src/product.rs src/index.rs src/graph.rs src/search.rs
+/workspaces/MegaStore/megastore_busca/target/debug/deps/libmegastore-d66525ef07236c54.rmeta: src/lib.rs src/product.rs src/index.rs src/graph.rs src/search.rs
 
 src/lib.rs:
 src/product.rs:
